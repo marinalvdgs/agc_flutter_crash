dc3f0670e362b5b6f06ad2969084ae165c557fd0
marinaivanova <h4ys-245Qe186TVZQoQa>
2022-05-20 11:16:26 +0600

    enable crash method

--- lib/agc_flutter_crash.dart
+++ lib/agc_flutter_crash.dart
@@ -1,6 +1,7 @@
 
 import 'dart:async';
 
+import 'package:agconnect_crash/agconnect_crash.dart';
 import 'package:flutter/services.dart';
 
 class AgcFlutterCrash {
@@ -9,5 +10,9 @@ class AgcFlutterCrash {
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  Future<void> enableCrash(bool enable) async {
+    await AGCCrash.instance.enableCrashCollection(enable);
   }
 }
